8cf1e54a91c57bd3f862d22a9c5c72f2af5f9e77
harleyricklin <harleyricklin@gmail.com>
2019-02-18 10:32:52 -0500

    add comments and formatting

--- photoSensorBlink/photoSensorBlink.ino
+++ photoSensorBlink/photoSensorBlink.ino
@@ -2,11 +2,10 @@
 int lightPin = A0;
 int ledPin = 13;
 int lastLevel = 0;
-int sensorPin = A1;
-int sensorValue = 0;
+int potPin = A1;
+int potValue = 0;
 
 void setup() {
-  // put your setup code here, to run once:
   // set the led pin to output
   pinMode(ledPin, OUTPUT);
   // turn on the serial monitor
@@ -17,6 +16,7 @@ void setup() {
 void loop() {
   // read in the light level
   int lightLevel = analogRead(lightPin);
+
   // map the lightlevel to 0<=lightlevel<=255
   lightLevel = map(lightLevel, 0, 900, 0, 255);
   if (lightLevel != lastLevel) {
@@ -24,11 +24,15 @@ void loop() {
     Serial.print("lightLevel mapped value: ");
     Serial.println(lightLevel);   
   }
-  // write the light level to the LED
+
+  // turn on the LED, write the light level to the LED
   analogWrite(ledPin, lightLevel); 
-  // read the value from the sensor:
-  sensorValue = analogRead(sensorPin);
-  delay(sensorValue);
+
+  // read the value from the sensor: 0 - 1023
+  potValue = analogRead(potPin);
+  delay(potValue);
+  
+  // only blink if the potentiometer is set below 1000
   if (sensorValue < 1000) {
     analogWrite(ledPin, 0);
     delay(sensorValue);
