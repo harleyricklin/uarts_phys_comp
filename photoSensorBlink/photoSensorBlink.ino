6d10c361277a37e8f8ff21bf2efdc22e8c488f02
Richard Lin <richardlin@richards-air.ua.lan>
2019-02-18 18:05:13 -0500

    add ability to turn only if dark

--- photoSensorBlink/photoSensorBlink.ino
+++ photoSensorBlink/photoSensorBlink.ino
@@ -19,30 +19,39 @@ void loop(){
 
   //// read in the light level
   int lightLevel = analogRead(lightPin);
-
+  Serial.print("lightLevel value: ");
+  Serial.print(lightLevel);   
+  
   //// map the lightlevel to 0<=lightlevel<=255
   mappedLevel = map(lightLevel, 0, 900, 0, 255);
-  Serial.print("lightLevel mapped value: ");
+  Serial.print(" : lightLevel mapped value: ");
   Serial.println(mappedLevel);   
 
-  //// turn the ledPin on, write the light level to the LED
-  analogWrite(ledPin, mappedLevel);
+  if ( lightLevel > 810) {
+    //// turn the ledPin on, write the light level to the LED
+    analogWrite(ledPin, mappedLevel);
+  
+  
+    // read the value from the sensor: 0 - 1023
+    potValue = analogRead(potPin);
+    Serial.print("pot value: ");
+    Serial.println(potValue);
 
-  // read the value from the sensor: 0 - 1023
-  potValue = analogRead(potPin);
-  Serial.print("pot value: ");
-  Serial.println(potValue);
-
-  //  WAIT : leave the LED on at this level for this amount of milliseconds
-  delay(potValue);
-
-  //// only blink if the potentiometer is set below 1000
-  if (potValue < 1000){
-
-    //// turn the ledPin off:
-    analogWrite(ledPin, 0);
-
-    // WAIT : leave the LED off for this amount of milliseconds
+    //  WAIT : leave the LED on at this level for this amount of milliseconds
     delay(potValue);
+
+    //// only blink if the potentiometer is set below 1000
+    if (potValue < 1000){
+
+      //// turn the ledPin off:
+      analogWrite(ledPin, 0);
+
+      // WAIT : leave the LED off for this amount of milliseconds
+      delay(potValue);
+    }
+  } else {
+      //// turn the ledPin off:
+      analogWrite(ledPin, 0);
+    
   }
 }
